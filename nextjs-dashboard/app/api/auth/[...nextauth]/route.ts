c65f7f75d4ddacdc0dbf638941e2d53861220163
Ezra-186 <ezrad186@Gmail.com>
2026-01-23 00:30:05 -0800

    FR

--- nextjs-dashboard/app/api/auth/[...nextauth]/route.ts
+++ nextjs-dashboard/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,12 @@
-import { handlers } from '@/auth';
 export const runtime = 'nodejs';
 
+import { handlers } from '@/auth';
+
+console.log(
+  'AUTH_SECRET set?',
+  Boolean(process.env.AUTH_SECRET),
+  'NEXTAUTH_SECRET set?',
+  Boolean(process.env.NEXTAUTH_SECRET)
+);
 
 export const { GET, POST } = handlers;
